4c2fbe2af1e6508bd0878d2cbbf2ba21076c8976
Sura-phat <suraphat.phu@mail.kmutt.ac.th>
2022-11-24 23:46:36 +0700

    Finish work

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<char> q1 = new Queue<char>();
+        char flowerself = 'f';
+        while(flowerself == 'J'|| flowerself =='G'|| flowerself == 'O'|| flowerself == 'R'|| flowerself == 'f')
+        {
+            flowerself = char.Parse(Console.ReadLine());
+        }
+    }
+}
